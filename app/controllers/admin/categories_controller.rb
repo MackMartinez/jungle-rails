073ff54f3c61258dbb5bd6ddf72a15fdc92e56ad
MackyClap <martimac2320@gmail.com>
2023-02-16 22:10:22 -0500

    add new view for categories and show form to enter name

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -7,4 +7,14 @@ class Admin::CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 end
