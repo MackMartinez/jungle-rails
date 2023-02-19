611d45b632d886fba87a2e18a85347319a63abf4
MackyClap <martimac2320@gmail.com>
2023-02-19 16:13:07 -0500

    validate user created if pw and pw confirmation do match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe User, type: :model do
       @user = User.new({ name: 'Jack Sparrow', email: 'jack@testmail.com', password: '123', password_confirmation: '321'})
       expect(@user).not_to be_valid
     end
+
+    it 'should create a suser if password and confirmation to match' do
+      @user = User.new({ name: 'Jack Sparrow', email: 'jack@testmail.com', password: '123', password_confirmation: '123'})
+      expect(@user).to be_valid
+    end
   end
 end
