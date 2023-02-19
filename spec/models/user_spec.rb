d292116809e76885ab200a68e460dbddb7ff1a24
MackyClap <martimac2320@gmail.com>
2023-02-19 16:16:37 -0500

    validate if user is not valid with email missing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,14 @@ RSpec.describe User, type: :model do
       expect(@user).not_to be_valid
     end
 
-    it 'should create a suser if password and confirmation to match' do
+    it 'should create a user if password and confirmation to match' do
       @user = User.new({ name: 'Jack Sparrow', email: 'jack@testmail.com', password: '123', password_confirmation: '123'})
       expect(@user).to be_valid
+    end
+
+    it 'should not create a user if no e-mail is entered' do
+      @user = User.new({name: nil, email: 'jack@testmail.com', password: '123', password_confirmation: '123'})
+      expect(@user).not_to be_valid
     end
   end
 end
