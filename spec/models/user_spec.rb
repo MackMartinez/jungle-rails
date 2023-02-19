7b76c5bf209f5cb01ef470a9ba0e032b5e7dc8db
MackyClap <martimac2320@gmail.com>
2023-02-19 16:11:14 -0500

    validate user not created if pw and pw confirmation don't match

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'should not create a user if password and confirmation do not match' do
+      @user = User.new({ name: 'Jack Sparrow', email: 'jack@testmail.com', password: '123', password_confirmation: '321'})
+      expect(@user).not_to be_valid
+    end
+  end
 end
