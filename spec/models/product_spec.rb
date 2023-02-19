1034171abd00bb65220a067488ea04c0d212a66a
MackyClap <martimac2320@gmail.com>
2023-02-19 12:44:06 -0500

    validate product is valid when all 4 fields set

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "should save a product with all four fields set" do
+      @category = Category.create(name: 'Shrubs')
+      @category.save
+
+      @product = Product.new
+      @product.name = 'Evergreen'
+      @product.price = 80
+      @product.quantity = 10
+      @product.category_id = @category.id
+      @product.save
+
+      expect(@product).to be_valid
+    end
+  end
 end
